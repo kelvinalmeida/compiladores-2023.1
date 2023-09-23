0a29bdeefc54a2a0c29ab5d5f6a3cca65f876119
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-09-23 11:41:04 -0300

    começando a abrir o navegador

--- sintatico.py
+++ sintatico.py
@@ -1,6 +1,10 @@
 
 import lexico
 import sys # Para parar o programa apos um erro.
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
 
 count = 0
 lookahead = lexico.palavrasTratadas[count]
@@ -68,42 +72,57 @@ def explore():
     global count
     presentList = ['link_pdf','link_video', 'endereço_meet']
 
-    lookaheadPreeditivo = lexico.palavrasTratadas[count + 1]
+
+    if(count + 1 < len(lexico.palavrasTratadas)):
+        lookaheadPreeditivo = lexico.palavrasTratadas[count + 1]
+    else:
+        return
 
     if(lookaheadPreeditivo in presentList):
         return
     elif(lookahead == 'navegador'):
         matchLookAhead('navegador')
         print('Executando o navegador')
-        tempo()
-        
+        tempo('navegador')
         # Recursão da explore
         explore()
     else:
         error()
 
-def tempo():
+def tempo(tipo):
     global lookahead
     if(lookahead == '20_min;'):
         matchLookAhead('20_min;')
         print('por 20_min')
+        abrirComandos(tipo, '20_min;')
     elif(lookahead == '1_dia;'):
         matchLookAhead('1_dia;')
         print('por 1_dia')
+        abrirComandos(tipo, '1_dia;')
     elif(lookahead == '2_dias;'):
         matchLookAhead('2_dias;')
         print('por 2_dias')
+        abrirComandos(tipo, '2_dias;')
     elif(lookahead == 'sem_limite;'):
         matchLookAhead('sem_limite;')
         print('por sem_limite')
     elif(lookahead == '15_min;'):
         matchLookAhead('15_min;')
         print('por 15_min')
+        abrirComandos(tipo, '15_min;')
+    elif(lookahead == '1_hora;'):
+        matchLookAhead('1_hora;')
+        print('por 1_hora')
+        abrirComandos(tipo, '1_hora;')
     else:
         error()
 
 def present():
     global lookahead
+    global count
+
+    if(count + 1 == len(lexico.palavrasTratadas)):
+        return
 
     if(lookahead == 'navegador'):
         matchLookAhead('navegador')
@@ -118,6 +137,7 @@ def present():
         elif(lookahead == 'endereço_meet'):
             matchLookAhead('endereço_meet')            
             print('executar endereço_meet.')
+            abrirVideoConferencia()
             tempo()
         else:
             error()
@@ -126,6 +146,12 @@ def present():
 
 def interact():
     global lookahead
+    global count
+
+
+    if(count + 1 == len(lexico.palavrasTratadas)):
+        return
+
     if(lookahead == 'navegador'):
         matchLookAhead('navegador')
         if(lookahead == 'link_whatsapp_web'):
@@ -140,6 +166,7 @@ def interact():
         elif(lookahead == 'endereço_meet'):
             matchLookAhead('endereço_meet')
             print('executar endereço_meet.')
+            abrirVideoConferencia()
             tempo()
         else:
             error()
@@ -148,6 +175,11 @@ def interact():
 
 def critique():
     global lookahead
+    global count
+
+    if(count + 1 == len(lexico.palavrasTratadas)):
+        return
+
     if(lookahead == 'navegador'):
         matchLookAhead('navegador')
         if(lookahead == 'link_whatsapp_web'):
@@ -162,6 +194,7 @@ def critique():
         elif(lookahead == 'endereço_meet'):
             matchLookAhead('endereço_meet')
             print('executar endereço_meet.')
+            abrirVideoConferencia()
             tempo()
         else:
             error()
@@ -182,6 +215,102 @@ def error():
     print('Erro Sintatico')
     print()
     sys.exit()
+
+
+def abrirComandos(tipo, tempo):
+    if(tipo == 'navegador'):
+        abrirNavegador(tempo)
+
+
+def abrirNavegador(tempo):
+    global driver
+    driver.get("https://google.com/")
+
+    if(tempo == '20_min;'):
+        time.sleep(10)
+    elif(tempo == '1_dia;'):
+        time.sleep(10)
+    elif(tempo == 'sem_limite;'):
+        time.sleep(10)
+    elif(tempo == '15_min;'):
+        time.sleep(10)
+    elif(tempo == '1_hora;'):
+        time.sleep(10)
+
+def abrirVideoConferencia(tempo):
+    global driver
+    driver.get("https://meet.google.com/")
+    
+    if(tempo == '20_min;'):
+        time.sleep(10)
+    elif(tempo == '1_dia;'):
+        time.sleep(10)
+    elif(tempo == 'sem_limite;'):
+        time.sleep(10)
+    elif(tempo == '15_min;'):
+        time.sleep(10)
+    elif(tempo == '1_hora;'):
+        time.sleep(10)
+
+def abrirWhatsappWeb(tempo):
+    global driver
+    driver.get("https://web.whatsapp.com/")
+    
+    if(tempo == '20_min;'):
+        time.sleep(10)
+    elif(tempo == '1_dia;'):
+        time.sleep(10)
+    elif(tempo == 'sem_limite;'):
+        time.sleep(10)
+    elif(tempo == '15_min;'):
+        time.sleep(10)
+    elif(tempo == '1_hora;'):
+        time.sleep(10)
+
+def abrirEmail(tempo):
+    global driver
+    driver.get("https://mail.google.com/")
+    
+    if(tempo == '20_min;'):
+        time.sleep(10)
+    elif(tempo == '1_dia;'):
+        time.sleep(10)
+    elif(tempo == 'sem_limite;'):
+        time.sleep(10)
+    elif(tempo == '15_min;'):
+        time.sleep(10)
+    elif(tempo == '1_hora;'):
+        time.sleep(10)
+
+def abrirPDF(tempo):
+    global driver
+    driver.get("https://inc.saude.gov.br/download/50_receitas.pdf")
+    
+    if(tempo == '20_min;'):
+        time.sleep(10)
+    elif(tempo == '1_dia;'):
+        time.sleep(10)
+    elif(tempo == 'sem_limite;'):
+        time.sleep(10)
+    elif(tempo == '15_min;'):
+        time.sleep(10)
+    elif(tempo == '1_hora;'):
+        time.sleep(10)
+
+def abrirVideo(tempo):
+    global driver
+    driver.get("https://www.youtube.com/")
+    
+    if(tempo == '20_min;'):
+        time.sleep(10)
+    elif(tempo == '1_dia;'):
+        time.sleep(10)
+    elif(tempo == 'sem_limite;'):
+        time.sleep(10)
+    elif(tempo == '15_min;'):
+        time.sleep(10)
+    elif(tempo == '1_hora;'):
+        time.sleep(10)
 
 
 # lexico()
